124c03942b689b50721ac90c0259b09b771b8704
mviniciuz <m.viniciuz@hotmail.com>
2020-09-02 18:27:22 -0300

    finished

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://167.172.238.228:3333',
 });
+
+// baseURL: 'http://167.172.238.228:3333',
+// baseURL: 'http://localhost:3333',
+// baseURL: 'http://192.168.99.100:3333',
